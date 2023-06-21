f8676bc6fdc57560dd4c4b23122af4baeeb9ac26
procrastimax <mmeheykeroth@protonmail.com>
2023-06-21 17:01:02 +0200

    Adds container script to train private models

---
+++ scripts/private-learning-container.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -J private-learning-single-model-container
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --partition=clara
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=1-00:00:00
+#SBATCH -o logs/%A-%x-%a.out
+#SBATCH -e logs/%A-%x-%a.error
+#SBATCH --mail-type=FAIL,END
+# #SBATCH --mail-user=
+
+ds='mnist cifar10gray'
+
+# get correct run for array id
+case $SLURM_ARRAY_TASK_ID in
+        0)
+                l2_norm_clip=1.0
+                eps=0.1
+                epochs=20
+                ;;
+        1)
+                l2_norm_clip=2.0
+                eps=0.1
+                epochs=20
+                ;;
+        2)
+                l2_norm_clip=1.0
+                eps=0.1
+                epochs=40
+                ;;
+        3)
+                l2_norm_clip=1.0
+                eps=0.1
+                epochs=60
+                ;;
+esac
+
+echo "$SLURM_ARRAY_TASK_ID" "-" "$ds" "-" "$eps"
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -r $SLURM_ARRAY_TASK_ID -e $eps -m private_small_cnn -c $l2_norm_clip --epochs $epochs --train-single-model --load-test-single-model
