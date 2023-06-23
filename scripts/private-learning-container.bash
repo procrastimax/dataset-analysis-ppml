04e85f914f16fefec3468b02454a313d3cc87ed9
procrastimax <mmeheykeroth@protonmail.com>
2023-06-23 15:05:34 +0200

    Modifies private-train script

--- scripts/private-learning-container.bash
+++ scripts/private-learning-container.bash
@@ -10,31 +10,39 @@
 #SBATCH --mail-type=FAIL,END
 # #SBATCH --mail-user=
 
-ds='mnist cifar10gray'
+ds='mnist cifar10'
 
 # get correct run for array id
 case $SLURM_ARRAY_TASK_ID in
         0)
                 l2_norm_clip=1.0
-                eps=0.1
+                eps=1.0
                 epochs=20
                 ;;
         1)
                 l2_norm_clip=2.0
-                eps=0.1
+                eps=1.0
                 epochs=20
                 ;;
         2)
                 l2_norm_clip=1.0
-                eps=0.1
-                epochs=40
+                eps=1.0
+                epochs=30
                 ;;
         3)
                 l2_norm_clip=1.0
                 eps=0.1
-                epochs=60
+                epochs=20
+                ;;
+        4)
+                l2_norm_clip=1.0
+                eps=10
+                epochs=20
                 ;;
 esac
 
+batch=250
+micro_batch=250
+
 echo "$SLURM_ARRAY_TASK_ID" "-" "$ds" "-" "$eps"
-srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -r $SLURM_ARRAY_TASK_ID -e $eps -m private_small_cnn -c $l2_norm_clip --epochs $epochs --train-single-model --load-test-single-model
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -r $SLURM_ARRAY_TASK_ID -e $eps -m private_small_cnn -c $l2_norm_clip --epochs $epochs --train-single-model --load-test-single-model --batch-size $batch -b $micro_batch
