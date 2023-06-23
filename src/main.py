1a10dd6df6d5f7df7c5e02b3b03541900d14248f
procrastimax <mmeheykeroth@protonmail.com>
2023-06-23 11:37:23 +0200

    Reduces default epochs

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ import json
 
 from model import SmallCNNModel, Model, PrivateSmallCNNModel
 
-epochs: int = 40
+epochs: int = 30
 batch: int = 250
 learning_rate: float = 0.001
 momentum: Optional[float] = None
