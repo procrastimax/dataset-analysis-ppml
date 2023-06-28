df45e2ca9536c43fe224e5724587816f5ffb5257
procrastimax <mmeheykeroth@protonmail.com>
2023-06-28 16:04:59 +0200

    Adds script to train private and non-private models

---
+++ scripts/non-private-attack-models-container.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J non-private-attacker
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --partition=clara
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=0-10:00:00
+#SBATCH -o logs/%A-%x-%a.out
+#SBATCH -e logs/%A-%x-%a.error
+#SBATCH --mail-type=FAIL,END
+
+ds='mnist_c5000 fmnist_c5000 cifar10_c5000 cifar10gray_c5000'
+shadow_models=32
+epochs=50
+model="small_cnn"
+batch=250
+
+
+echo "non-private attacking" "$ds" - "$model"
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -s $shadow_models -r 0 --epochs $epochs --batch-size $batch --run-amia-attack --generate-results --force-model-retrain
---
+++ scripts/private-attack-models-container.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -J private-attacker
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --partition=clara
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=0-15:00:00
+#SBATCH -o logs/%A-%x-%a.out
+#SBATCH -e logs/%A-%x-%a.error
+#SBATCH --mail-type=FAIL,END
+
+ds='mnist_c5000 fmnist_c5000 cifar10_c5000 cifar10gray_c5000'
+shadow_models=32
+epochs=15
+model="private_small_cnn"
+batch=250
+
+# get correct run for array id
+case $SLURM_ARRAY_TASK_ID in
+        0)
+                eps=1.0
+                ;;
+        1)
+                eps=30
+                ;;
+esac
+
+echo "private-attacking" "$SLURM_ARRAY_TASK_ID" "-" "$ds" - "$model"
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -e $eps -d $ds -m $model -s $shadow_models -r $SLURM_ARRAY_TASK_ID --epochs $epochs -b $batch --batch-size $batch --run-amia-attack --generate-results --force-model-retrain
