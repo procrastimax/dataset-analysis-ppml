67d36519e1dc31c9800ef7c9a11a538bdd37a026
procrastimax <mmeheykeroth@protonmail.com>
2023-07-25 21:24:31 +0200

    Adds script for hyperparameter evaluation

---
+++ scripts/197-non-private-learning-rate-comparison.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH -J non-private-learning-rate-comparison
+#SBATCH --ntasks=1
+#SBATCH --mem=40G
+#SBATCH --partition=clara
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=0-5:00:00
+#SBATCH -o logs/%A-%x-%a.out
+#SBATCH -e logs/%A-%x-%a.error
+#SBATCH --mail-type=FAIL
+
+ds='mnist fmnist svhn cifar10'
+model="cnn"
+batch=600
+run_name="non-private-learning-rate-comparison"
+epochs=30
+
+# get correct run for array id
+case $SLURM_ARRAY_TASK_ID in
+        0)
+                lr=0.0001
+                ;;
+        1)
+                lr=0.001
+                ;;
+        2)
+                lr=0.01
+                ;;
+        3)
+                lr=0.1
+                ;;
+        4)
+                lr=1.0
+                ;;
+esac
+
+echo "non-private" "$SLURM_ARRAY_TASK_ID" "-" "$ds" - "$model"
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -r $SLURM_ARRAY_TASK_ID --epochs $epochs --batch-size $batch -n $run_name -tm -em -l $lr
