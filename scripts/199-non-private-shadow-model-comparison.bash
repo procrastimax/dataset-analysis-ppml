137a3013c15cd742ce126c065962026220b6425b
procrastimax <mmeheykeroth@protonmail.com>
2023-08-03 15:12:38 +0200

    Adapt script 199 to programmed default values

--- scripts/199-non-private-shadow-model-comparison.bash
+++ scripts/199-non-private-shadow-model-comparison.bash
@@ -4,31 +4,29 @@
 #SBATCH --mem=40G
 #SBATCH --partition=clara
 #SBATCH --gres=gpu:v100:1
-#SBATCH --time=0-20:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH -o logs/%A-%x-%a.out
 #SBATCH -e logs/%A-%x-%a.error
 #SBATCH --mail-type=FAIL
 
 ds='mnist fmnist svhn cifar10'
-epochs=30
 model="cnn"
-batch=600
+run_name="non-private-shadow-model-comparison"
 
 # get correct run for array id
 case $SLURM_ARRAY_TASK_ID in
-        0)
-                shadow_models=8
-                ;;
-        1)
-                shadow_models=16
-                ;;
-        2)
-                shadow_models=32
-                ;;
-        3)
-                shadow_models=64
-                ;;
+    0)
+        shadow_models=8
+        ;;
+    1)
+        shadow_models=16
+        ;;
+    2)
+        shadow_models=32
+        ;;
+    3)
+        shadow_models=64
+        ;;
 esac
 
-echo "private-attacking" "$SLURM_ARRAY_TASK_ID" "-" "$ds" - "$model"
-srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -s $shadow_models -r $SLURM_ARRAY_TASK_ID --epochs $epochs --batch-size $batch --run-amia-attack --include-mia --generate-results --force-model-retrain -n "non-private-shadow-model-comparison"
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -s $shadow_models -r $SLURM_ARRAY_TASK_ID --run-amia-attack --include-mia --generate-results --force-model-retrain -n $run_name
