6c078f604d7229708e146a922ed3229b4b264f87
procrastimax <mmeheykeroth@protonmail.com>
2023-08-05 03:24:20 +0200

    Update older scripts 10, 20

--- scripts/10-non-private-attack-c5000.bash
+++ scripts/10-non-private-attack-c5000.bash
@@ -10,11 +10,7 @@
 #SBATCH --mail-type=FAIL
 
 ds='mnist_c5000 fmnist_c5000 cifar10_c5000 cifar10_c5000_gray svhn_c5000 svhn_c5000_gray'
-shadow_models=32
-epochs=50
 model="cnn"
-batch=200
 run_name="non-private-attack-c5000"
 
-echo $run_name "$ds" - "$model"
-srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -s $shadow_models -r 0 --epochs $epochs --batch-size $batch --run-amia-attack --generate-results --force-model-retrain -n $run_name
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -r 0 --run-amia-attack --force-model-retrain -n $run_name
--- scripts/20-private-attack-c5000.bash
+++ scripts/20-private-attack-c5000.bash
@@ -10,21 +10,17 @@
 #SBATCH --mail-type=FAIL
 
 ds='mnist_c5000 fmnist_c5000 cifar10_c5000 cifar10_c5000_gray svhn_c5000 svhn_c5000_gray'
-shadow_models=32
-epochs=10
 model="private_cnn"
-batch=200
 run_name="private-attack-c5000"
 
 # get correct run for array id
 case $SLURM_ARRAY_TASK_ID in
-        0)
-                eps=1
-                ;;
-        1)
-                eps=30
-                ;;
+    0)
+        eps=1
+        ;;
+    1)
+        eps=30
+        ;;
 esac
 
-echo $run_name "$SLURM_ARRAY_TASK_ID" "-" "$ds" - "$model"
-srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -e $eps -d $ds -m $model -s $shadow_models -r $SLURM_ARRAY_TASK_ID --epochs $epochs -b $batch --batch-size $batch --run-amia-attack --generate-results --force-model-retrain -n $run_name
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -e $eps -d $ds -m $model r $SLURM_ARRAY_TASK_ID --run-amia-attack --force-model-retrain -n $run_name
