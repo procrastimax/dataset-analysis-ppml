f7cd5f49b000134abf04103e79bd2c11e51fc387
procrastimax <mmeheykeroth@protonmail.com>
2023-08-21 10:41:20 +0200

    Adds _c mod to cifar100

--- src/ppml_datasets/datasets/cifar100.py
+++ src/ppml_datasets/datasets/cifar100.py
@@ -4,11 +4,13 @@ import tensorflow as tf
 
 from ppml_datasets.abstract_dataset_handler import (
     AbstractDataset,
+    AbstractDatasetClassSize,
     AbstractDatasetCustomClasses,
 )
 
 
 class Cifar100Dataset(AbstractDataset):
+
     def __init__(
         self,
         model_img_shape: Tuple[int, int, int],
@@ -37,6 +39,7 @@ class Cifar100Dataset(AbstractDataset):
 
 
 class Cifar100DatasetCustomClasses(AbstractDatasetCustomClasses):
+
     def __init__(self, ds: Cifar100Dataset, new_num_classes: int):
         self.new_num_classes = new_num_classes
         self.ds_train = ds.ds_train
@@ -48,6 +51,29 @@ class Cifar100DatasetCustomClasses(AbstractDatasetCustomClasses):
             dataset_name=f"{ds.dataset_name}_n{self.new_num_classes}",
             dataset_path=ds.dataset_path,
             dataset_img_shape=ds.dataset_img_shape,
+            model_img_shape=ds.model_img_shape,
+            batch_size=ds.batch_size,
+            convert_to_rgb=ds.convert_to_rgb,
+            augment_train=ds.augment_train,
+            shuffle=ds.shuffle,
+            is_tfds_ds=ds.is_tfds_ds,
+            builds_ds_info=ds.builds_ds_info,
+        )
+
+
+class Cifar100DatasetClassSize(AbstractDatasetClassSize):
+
+    def __init__(self, ds: Cifar100Dataset, class_size: int):
+        self.class_size = class_size
+        self.ds_train = ds.ds_train
+        self.ds_val = ds.ds_val
+        self.ds_test = ds.ds_test
+        super().__init__(
+            tfds_name=ds.tfds_name,
+            num_classes=ds.num_classes,
+            dataset_img_shape=ds.dataset_img_shape,
+            dataset_name=f"{ds.dataset_name}_c{class_size}",
+            dataset_path=ds.dataset_path,
             model_img_shape=ds.model_img_shape,
             batch_size=ds.batch_size,
             convert_to_rgb=ds.convert_to_rgb,
@@ -75,7 +101,13 @@ def build_cifar100(
 
     if "n" in mods:
         num_new_classes = mods["n"][0]
-        ds = Cifar100DatasetCustomClasses(ds=ds, new_num_classes=num_new_classes)
+        ds = Cifar100DatasetCustomClasses(ds=ds,
+                                          new_num_classes=num_new_classes)
+        ds.load_dataset()
+
+    if "c" in mods:
+        class_size = mods["c"][0]
+        ds = Cifar100DatasetClassSize(ds, class_size=class_size)
         ds.load_dataset()
 
     return ds
