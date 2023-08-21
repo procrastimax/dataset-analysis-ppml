19025eb8c09853f9d5f7a0da98a8f9f4f6f6e4fe
procrastimax <mmeheykeroth@protonmail.com>
2023-08-21 12:07:19 +0200

    Adds scripts for class count comparison emnist, cifar100

---
+++ scripts/23-private-class-count-comparison.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH -J private-class-count-comparison
+#SBATCH --ntasks=1
+#SBATCH --mem=50G
+#SBATCH --partition=clara
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=2-00:00:00
+#SBATCH -o logs/%A-%x-%a.out
+#SBATCH -e logs/%A-%x-%a.error
+#SBATCH --mail-type=FAIL
+
+run_name="private-class-count-comparison"
+model="private_cnn"
+
+case $SLURM_ARRAY_TASK_ID in
+    0)
+        ds='emnist_n47 cifar100_n47'
+        ;;
+    1)
+        ds='emnist_n42 cifar100_n42'
+        ;;
+    2)
+        ds='emnist_n37 cifar100_n37'
+        ;;
+    3)
+        ds='emnist_n32 cifar100_n32'
+        ;;
+    4)
+        ds='emnist_n27 cifar100_n27'
+        ;;
+    5)
+        ds='emnist_n22 cifar100_n22'
+        ;;
+    6)
+        ds='emnist_n17 cifar100_n17'
+        ;;
+    7)
+        ds='emnist_n12 cifar100_n12'
+        ;;
+    8)
+        ds='emnist_n7 cifar100_n7'
+        ;;
+    9)
+        ds='emnist_n3 cifar100_n3'
+        ;;
+esac
+
+srun singularity exec --nv container-dataset-analysis.sif python3.9 src/main.py -d $ds -m $model -r $SLURM_ARRAY_TASK_ID --run-amia-attack --force-model-retrain -n $run_name
