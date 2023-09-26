c8b370c477128dbedc739645aa2466b83c00b208
procrastimax <mmeheykeroth@protonmail.com>
2023-09-26 15:40:24 +0200

    Fix graph generation for compiled model evaluation

--- src/analyser.py
+++ src/analyser.py
@@ -484,11 +484,7 @@ class UtilityAnalyser:
         return df
 
     def analyse_utility(self):
-        utility_df = self.build_combined_model_utility_df()
-
-        acc_df = utility_df["accuracy"]
-        f1_df = utility_df["f1-score"]
-        loss_df = utility_df["loss"]
+        acc_df, f1_df, loss_df = self.build_combined_model_utility_dfs()
 
         ###
         # Accuracy
@@ -541,30 +537,57 @@ class UtilityAnalyser:
         loss_fig.savefig(loss_vis_filename)
         save_dataframe(loss_df, loss_df_filename)
 
-    def build_combined_model_utility_df(self) -> pd.DataFrame:
-        dfs: List[pd.DataFrame] = []
-        for run in self.run_numbers:
-            run_df: pd.DataFrame = self.load_run_utility_df(run)
-            run_df["run"] = run
-            col = run_df["run"]
-            run_df.drop(labels=["run"], axis=1, inplace=True)
-            run_df.insert(0, "run", col)
-            dfs.append(run_df)
+    def build_combined_model_utility_dfs(self) -> Tuple[pd.DataFrame]:
+        """Return a tuple of 3 dataframes. Each dataframes represents one utility anaylsis from the evaluated model.
 
-        combined_df = pd.concat(dfs, axis=0, ignore_index=True)
-        combined_df = combined_df.pivot(
-            index=["name", "type"],
-            columns="run",
-            values=["accuracy", "f1-score", "loss"],
-        )
-        averaged = combined_df.groupby("type").mean()
-        averaged.rename(index={
-            "test": "average test",
-            "train": "average train"
-        },
-                        inplace=True)
-        combined_df = pd.concat([combined_df, averaged])
-        return combined_df
+        The dataframes have the following order in the tuple:
+            1 -> Accuracy
+            2 -> F1-Score
+            3 -> Loss
+        """
+        # here we have a bug in the program
+        # every utility df from the '-em' cli parameter saves the complete utility values from all previous runs
+        # so loading only the last run's utility df is enough to get all utility data
+        last_run = self.run_numbers[-1]
+        run_df: pd.DataFrame = self.load_run_utility_df(last_run)
+
+        # this dict holds the name of the dataset as keys (cifar10_c52 and cifar10_30 are just cifar10)
+        # to the keys the dict holds a list, one list entry is a tuple of (train value, test value)
+        # each list entry represents one experiment
+        run_dict_accuracy: Dict[str, List[float]] = defaultdict(list)
+        run_dict_f1score: Dict[str, List[float]] = defaultdict(list)
+        run_dict_loss: Dict[str, List[float]] = defaultdict(list)
+
+        for i in run_df.iterrows():
+            ds_name = i[1]["name"]
+            if "_" in ds_name:
+                ds_base_name = ds_name.split("_")[0]
+                if i[1]["type"] == "train":
+                    run_dict_accuracy[ds_base_name + "_train"].append(
+                        i[1]["accuracy"])
+                    run_dict_f1score[ds_base_name + "_train"].append(
+                        i[1]["f1-score"])
+                    run_dict_loss[ds_base_name + "_train"].append(i[1]["loss"])
+                elif i[1]["type"] == "test":
+                    run_dict_accuracy[ds_base_name + "_test"].append(
+                        i[1]["accuracy"])
+                    run_dict_f1score[ds_base_name + "_test"].append(
+                        i[1]["f1-score"])
+                    run_dict_loss[ds_base_name + "_test"].append(i[1]["loss"])
+
+        df_acc = pd.DataFrame.from_dict(run_dict_accuracy)
+        df_acc["avg_train"] = df_acc.filter(like="_train").mean(axis=1)
+        df_acc["avg_test"] = df_acc.filter(like="_test").mean(axis=1)
+
+        df_f1 = pd.DataFrame.from_dict(run_dict_f1score)
+        df_f1["avg_train"] = df_f1.filter(like="_train").mean(axis=1)
+        df_f1["avg_test"] = df_f1.filter(like="_test").mean(axis=1)
+
+        df_loss = pd.DataFrame.from_dict(run_dict_loss)
+        df_loss["avg_train"] = df_loss.filter(like="_train").mean(axis=1)
+        df_loss["avg_test"] = df_loss.filter(like="_test").mean(axis=1)
+
+        return (df_acc, df_f1, df_loss)
 
     def _visualize_df(
         self,
@@ -576,15 +599,22 @@ class UtilityAnalyser:
         use_legend: bool = True,
     ) -> matplotlib.figure.Figure:
         fig, ax = plt.subplots(figsize=(10, 10))
-        for index, row in df.iterrows():
-            if type(index) == tuple:
-                index = " ".join(index)
-            ax.plot(range(len(row)), row, label=index)
+        for name, values in df.items():
+            # dont include the average values in the graph
+            if name.startswith("avg"):
+                ax.plot(
+                    range(len(values)),
+                    values,
+                    label=name,
+                    linestyle="dotted",
+                    linewidth=2,
+                )
+            else:
+                ax.plot(range(len(values)), values, label=name)
 
         ax.set(xlabel=xLabel, ylabel=yLabel, title=title)
         ax.legend()
         plt.legend(loc=(1.04, 0))
-        plt.xticks(range(len(row)))
         plt.subplots_adjust(right=0.72)
         ax.grid()
         return fig
