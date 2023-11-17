81f35aa73ea29adb2f8ce21facccd4247d020680
procrastimax <mmeheykeroth@protonmail.com>
2023-11-17 13:28:08 +0100

    Adds class wise plot generation

--- helper/ds-info-visualizer.py
+++ helper/ds-info-visualizer.py
@@ -3,8 +3,12 @@ import os
 import sys
 import json
 import matplotlib.pyplot as plt
+import numpy as np
+from typing import Dict, List
+from collections import defaultdict
 
-FIGSIZE = (5, 5)
+plt.rcParams["figure.figsize"] = [5, 3]
+plt.rcParams["figure.autolayout"] = True
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -45,7 +49,6 @@ info_key = args.parameter
 is_class_wise = args.is_class_wise
 y_name = args.yName
 
-
 # get current script dir
 curr_dir = os.path.abspath(os.getcwd())
 
@@ -60,16 +63,21 @@ for f in file_names:
     with open(f, "r") as f:
         data = json.load(f)
         if info_key not in data:
+            print("------------------------------")
             print(f"Could not find key {info_key} in ds-info.json!")
+            print("------------------------------")
             sys.exit(1)
         ds_info_list.append(data)
 
 if not is_class_wise:
-    fig, ax = plt.subplots()
+    print(" --- Plotting Average Metric ---")
+    fig, ax = plt.subplots(layout="constrained")
     name_list = []
     value_list = []
     for info in ds_info_list:
         name = info["name"]
+        if name == "emnist-medium-balanced":
+            name = "emnist"
         value = info[info_key]
         name_list.append(name)
         value_list.append(value)
@@ -83,3 +91,38 @@ if not is_class_wise:
 
     plt.savefig(figure_filename)
 
+else:
+    print(" --- Plotting Class Wise Metric ---")
+    fig, ax = plt.subplots(layout="constrained")
+
+    value_dict: Dict[str, List[float]] = defaultdict(list)
+
+    for info in ds_info_list:
+        name = info["name"]
+        if name == "emnist-medium-balanced":
+            name = "emnist"
+
+        class_value_dict = info[info_key]
+        class_value_dict = dict(sorted(class_value_dict.items()))
+        value_dict[name] = list(class_value_dict.values())
+
+    x = np.arange(len(list(value_dict.values())[0]))
+    width = 0.22
+    multiplier = 0
+
+    figure_filename = (
+        f"class_wise_bar_chart_{info_key}_{'_'.join(value_dict.keys())}.png")
+
+    for ds_name, values in value_dict.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, values, width, label=ds_name)
+        multiplier += 1
+
+    if y_name is None:
+        ax.set_ylabel(info_key)
+    else:
+        ax.set_ylabel(y_name)
+    ax.set_xlabel("Class")
+    ax.set_xticks(x + width, x)
+    ax.legend()
+    plt.savefig(figure_filename)
