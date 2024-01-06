f08f6d9d929ec39b0cc852c66e3cf5d12e1e80c6
procrastimax <mmeheykeroth@protonmail.com>
2024-01-06 21:11:42 +0100

    Changes script to visualize class distribution

--- helper/visualize-dataset-normal-imbalance.py
+++ helper/visualize-dataset-normal-imbalance.py
@@ -1,5 +1,11 @@
+"""Simple script to visualize the distribution of samples in a dataset.
+
+Expects the ds_info.json file with the "class_counts" field.
+"""
+
 import json
 import sys
+
 import matplotlib.pyplot as plt
 
 filename = sys.argv[1]
@@ -8,18 +14,25 @@ print(f"Loading {filename}")
 class_sizes = []
 class_names = []
 
+ds_full_name = "dataset"
+
 with open(filename) as f:
-    classes = json.load(f)
-    for k, v in classes.items():
-        class_sizes.append(v)
-        class_names.append(int(k))
+    ds_info = json.load(f)
+    if "class_counts" in ds_info:
+        classes = ds_info["class_counts"]
+        for k, v in classes.items():
+            class_sizes.append(v)
+            class_names.append(int(k))
+
+    if "name" in ds_info:
+        ds_full_name = ds_info["name"]
 
 filename = filename.split("/")[-1]
 
-plt.rcParams['figure.constrained_layout.use'] = True
-plt.figure(figsize=(5, 3))
+plt.rcParams["figure.constrained_layout.use"] = True
+plt.figure(figsize=(4, 3))
 plt.bar(range(len(class_sizes)), class_sizes, color="royalblue", alpha=0.9)
 plt.xticks(class_names)
 plt.xlabel("Class")
-plt.ylabel("Number of Samples")
-plt.savefig(f"bar_plot_{filename}.png")
+plt.ylabel("# Samples")
+plt.savefig(f"class_sample_bar_{ds_full_name}.png")
