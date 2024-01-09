47fb313fd0368002f2839e1c0c9ba9218b8d9ab2
procrastimax <mmeheykeroth@protonmail.com>
2024-01-09 20:34:26 +0100

    Fixes X-axis labels for generate-all-graphics script

--- helper/generate-all-graphics.bash
+++ helper/generate-all-graphics.bash
@@ -2,63 +2,45 @@
 
 # Dataset Size Experiment
 # non-private
-python src/main.py -m cnn -n non-private-dataset-size-comparison -ar 0-7 --xName "# of Samples" --xValues 5000 4000 3000 2000 1000 500 100 50 -ca
-python src/main.py -m cnn -n non-private-dataset-size-comparison -ar 0-7 --xName "# of Samples" --xValues 5000 4000 3000 2000 1000 500 100 50 -ce
+python src/main.py -m cnn -n non-private-dataset-size-comparison -ar 0-7 --xName "# Samples per Class" --xValues 5000 4000 3000 2000 1000 500 100 50 -ca -ce
 # e=30
-python src/main.py -m private_cnn -n private-dataset-size-comparison-eps-30 -ar 0-7 --xName "# of Samples" --xValues 5000 4000 3000 2000 1000 500 100 50 -ce
-python src/main.py -m private_cnn -n private-dataset-size-comparison-eps-30 -ar 0-7 --xName "# of Samples" --xValues 5000 4000 3000 2000 1000 500 100 50 -ca
+python src/main.py -m private_cnn -n private-dataset-size-comparison-eps-30 -ar 0-7 --xName "# Samples per Class" --xValues 5000 4000 3000 2000 1000 500 100 50 -ca -ce
 # e=1
-python src/main.py -m private_cnn -n private-dataset-size-comparison-eps-1 -ar 0-7 --xName "# of Samples" --xValues 5000 4000 3000 2000 1000 500 100 50 -ce
-python src/main.py -m private_cnn -n private-dataset-size-comparison-eps-1 -ar 0-7 --xName "# of Samples" --xValues 5000 4000 3000 2000 1000 500 100 50 -ca
+python src/main.py -m private_cnn -n private-dataset-size-comparison-eps-1 -ar 0-7 --xName "# Samples per Class" --xValues 5000 4000 3000 2000 1000 500 100 50 -ca -ce
 
 # Dataset Class Count Experiment - 47 classes
 # non-private
-python src/main.py -m cnn -n non-private-class-count-comparison -ar 0-9 --xName "# of Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ca
-python src/main.py -m cnn -n non-private-class-count-comparison -ar 0-9 --xName "# of Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ce
+python src/main.py -m cnn -n non-private-class-count-comparison -ar 0-9 --xName "# Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ca -ce
 # e=30
-python src/main.py -m private_cnn -n private-class-count-comparison-eps-30 -ar 0-9 --xName "# of Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ca
-python src/main.py -m private_cnn -n private-class-count-comparison-eps-30 -ar 0-9 --xName "# of Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ce
+python src/main.py -m private_cnn -n private-class-count-comparison-eps-30 -ar 0-9 --xName "# Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ca -ce
 # e=1
-python src/main.py -m private_cnn -n private-class-count-comparison-eps-1 -ar 0-9 --xName "# of Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ca
-python src/main.py -m private_cnn -n private-class-count-comparison-eps-1 -ar 0-9 --xName "# of Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ce
+python src/main.py -m private_cnn -n private-class-count-comparison-eps-1 -ar 0-9 --xName "# Classes" --xValues 47 42 37 32 27 22 17 12 7 3 -ca -ce
 
 # Dataset Class Count Experiment - 10 classes
 # non-private
-python src/main.py -m cnn -n non-private-class-count-comparison-2 -ar 0-7 --xName "# of Classes" --xValues 10 9 8 7 6 5 4 3 -ca
-python src/main.py -m cnn -n non-private-class-count-comparison-2 -ar 0-7 --xName "# of Classes" --xValues 10 9 8 7 6 5 4 3 -ce
+python src/main.py -m cnn -n non-private-class-count-comparison-2 -ar 0-7 --xName "# Classes" --xValues 10 9 8 7 6 5 4 3 -ca -ce
 # e=30
-python src/main.py -m private_cnn -n private-class-count-comparison-2-eps-30 -ar 0-7 --xName "# of Classes" --xValues 10 9 8 7 6 5 4 3 -ca
-python src/main.py -m private_cnn -n private-class-count-comparison-2-eps-30 -ar 0-7 --xName "# of Classes" --xValues 10 9 8 7 6 5 4 3 -ce
+python src/main.py -m private_cnn -n private-class-count-comparison-2-eps-30 -ar 0-7 --xName "# Classes" --xValues 10 9 8 7 6 5 4 3 -ca -ce
 # e=1
-python src/main.py -m private_cnn -n private-class-count-comparison-2-eps-1 -ar 0-7 --xName "# of Classes" --xValues 10 9 8 7 6 5 4 3 -ca
-python src/main.py -m private_cnn -n private-class-count-comparison-2-eps-1 -ar 0-7 --xName "# of Classes" --xValues 10 9 8 7 6 5 4 3 -ce
+python src/main.py -m private_cnn -n private-class-count-comparison-2-eps-1 -ar 0-7 --xName "# Classes" --xValues 10 9 8 7 6 5 4 3 -ca -ce
 
 # Dataset Imbalance Experiment - Normal Mode
 # non-private
-python src/main.py -m cnn -n non-private-imbalance-comparison-n -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca
-python src/main.py -m cnn -n non-private-imbalance-comparison-n -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ce
+python src/main.py -m cnn -n non-private-imbalance-comparison-n -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca -ce
 # e=30
-python src/main.py -m private_cnn -n private-imbalance-comparison-n-eps-30 -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca
-python src/main.py -m private_cnn -n private-imbalance-comparison-n-eps-30 -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ce
+python src/main.py -m private_cnn -n private-imbalance-comparison-n-eps-30 -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca -ce
 # e=1
-python src/main.py -m private_cnn -n private-imbalance-comparison-n-eps-1 -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca
-python src/main.py -m private_cnn -n private-imbalance-comparison-n-eps-1 -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ce
+python src/main.py -m private_cnn -n private-imbalance-comparison-n-eps-1 -ar 0-4 --xName "Imbalance Factor (Normal Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca -ce
 
 # Dataset Imbalance Experiment - Linear Mode
 # non-private
-python src/main.py -m cnn -n non-private-imbalance-comparison-l -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca
-python src/main.py -m cnn -n non-private-imbalance-comparison-l -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ce
+python src/main.py -m cnn -n non-private-imbalance-comparison-l -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca -ce
 # e=30
-python src/main.py -m private_cnn -n private-imbalance-comparison-l-eps-30 -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca
-python src/main.py -m private_cnn -n private-imbalance-comparison-l-eps-30 -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ce
+python src/main.py -m private_cnn -n private-imbalance-comparison-l-eps-30 -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca -ce
 # e=1
-python src/main.py -m private_cnn -n private-imbalance-comparison-l-eps-1 -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca
-python src/main.py -m private_cnn -n private-imbalance-comparison-l-eps-1 -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ce
+python src/main.py -m private_cnn -n private-imbalance-comparison-l-eps-1 -ar 0-4 --xName "Imbalance Factor (Linear Mode)" --xValues 0.0 0.1 0.3 0.6 0.9 -ca -ce
 
 # Data Level Investigation
-python src/main.py -m cnn -n evaluation-basic-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ca
-python src/main.py -m cnn -n evaluation-basic-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ce
-python src/main.py -m cnn -n evaluation-color-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ca
-python src/main.py -m cnn -n evaluation-color-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ce
-python src/main.py -m cnn -n evaluation-complete-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ca
-python src/main.py -m cnn -n evaluation-complete-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ce
+python src/main.py -m cnn -n evaluation-basic-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ca -ce
+python src/main.py -m cnn -n evaluation-color-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ca -ce
+python src/main.py -m cnn -n evaluation-complete-ds -ar 0-2 --xName "Privacy Budget" --xValues inf 30 1 -ca -ce
